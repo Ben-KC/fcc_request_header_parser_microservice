f627b21244b8f6826671c33f86d1f097b633f635
Benjamin Cox <benny24c@gmail.com>
2016-11-06 03:56:36 +0000

    it at least works in Firefox

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ app.set('port', (process.env.PORT || 8080));
 
 //handle get
 app.get('/', function(req, res) {
-
+    console.log(req.headers);
+    var idObj = {
+        ipaddress: req.headers['x-forwarded-for'],
+        language: req.headers['accept-language'].split(',')[0],
+        software: req.headers['user-agent'].match(/\(([\w\s_.:;]+)\)/)[1]
+    };
+    
+    res.send(JSON.stringify(idObj));
 });
 
-app.listen(app.get(port));
+app.listen(app.get('port'));
